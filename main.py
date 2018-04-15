89c0d072baa42b008cce890c00cca4e403b59b4c
Yury Belousov <yury-belousov@yandex.ru>
2018-04-15 05:25:57 +0300

    basic version of api
    Former-commit-id: 46f9764fb7f5b5e1669dbd2c07ed6b9dda62ef63 [formerly 6eb6a3bc4636c8a30c94e1974b51b9ffc99c2975]
    Former-commit-id: a622ebf101a8f1ca269818dc25c65814fed29e10

--- main.py
+++ main.py
@@ -1,14 +1,57 @@
-from flask import Flask
+import matplotlib
+matplotlib.use('Agg')
+from flask import Flask, request, send_file
+from flask.json import jsonify
 import detect_logo
+import os
+import io
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "images"
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def bad_request(reason):
+    response = jsonify({"error": reason})
+    response.status_code = 400
+    return response
+
+
+@app.route('/api/upload', methods=['POST'])
+def upload_file():
+    try:
+        if 'file' not in request.files:
+            return bad_request("empty body")
+        file = request.files['file']
+        if file.filename == '':
+            return bad_request("empty filename")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            img = detect_logo.main(
+                filename, graph_params=graph_params, sess=sess)
+            print("lol")
+            return send_file(io.BytesIO(img.read()), mimetype='image/jpg')
+    except Exception as e:
+        bad_request(e)
+
 
 @app.route('/')
 def hello_world():
-  try:
-    predictor = detect_logo.main('test.jpg')
-  except Exception as e:
-    return e
-  return predictor + "from yury"
+    predictor = detect_logo.main(
+        'test.jpg', graph_params=graph_params, sess=sess)
+    return predictor
+
 
 if __name__ == '__main__':
-  app.run()
+    os.makedirs("images", exist_ok=True)
+    graph_params, sess = detect_logo.init_tf()
+    app.run()
