516536598c72be433f976ee889a7e8035f6870a0
Yury Belousov <yury-belousov@yandex.ru>
2018-04-15 06:06:25 +0300

    try to figure out where is the problem
    Former-commit-id: 0a983abc291cce7dead9a80a732afebc8f00dac9 [formerly 522c402f57a68240ecb596dd02ba3b3f76c27ce8]
    Former-commit-id: 6125504c7b39edc4fc433cdb50b74d5bfcbfe9a0

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def upload_file():
             # img = detect_logo.main(
             #     filename, graph_params=graph_params, sess=sess)
             print("lol")
-            return "hello"
+            return "hello new"
             #return send_file(io.BytesIO(img.read()), mimetype='image/jpg')
     except Exception as e:
         bad_request(e)
@@ -47,11 +47,12 @@ def upload_file():
 
 @app.route('/')
 def hello_world():
+    graph_params, sess = detect_logo.init_tf()
     predictor = detect_logo.main(
         'test.jpg', graph_params=graph_params, sess=sess)
     return predictor
 
 
 if __name__ == '__main__':
-    graph_params, sess = detect_logo.init_tf()
+    
     app.run()
